3b8df4f57c67c6d48f47c5823266150a71419897
SzymonPupel123 <166720268+SzymonPupel123@users.noreply.github.com>
2024-04-18 13:07:06 +0200

    feat added functionality

---
+++ models/data.py
@@ -0,0 +1,7 @@
+users:list[dict]=[
+    {"name":"Jan","surname":"Mielec","posts": 20},
+    {"name":"Maciej","surname":"Przybytek","posts": 45},
+    {"name":"Bartosz","surname":"Pietrasik","posts": 25},
+    {"name":"Tymoteusz","surname":"Miszczak","posts": 60},
+    {"name":"Mateusz","surname":"Matysiak","posts": 30},
+]
